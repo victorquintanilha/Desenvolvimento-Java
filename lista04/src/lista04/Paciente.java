4faf9d79f3e15b404ad26cb252aeddca5c404868
Victor Juan Bernardo Quintanilha <victorjuan_7@hotmail.com>
2019-04-02 22:21:08 -0300

    Aula 02/04/2019

---
+++ lista04/src/lista04/Paciente.java
@@ -0,0 +1,54 @@
+package lista04;
+
+import java.util.ArrayList;
+
+/**
+ * Classe para representar um paciente em um compromisso.
+ * 
+ * @author 1829203
+ *
+ */
+public class Paciente {
+
+	private String codigo;
+	
+	private String nome;
+	
+	private ArrayList<String> doencas;
+	
+	public Paciente(String codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.doencas = new ArrayList<>();
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public ArrayList<String> getHistorico() {
+		return this.doencas;
+	}
+	
+	public void addHistorico(String doenca) {
+		
+		//if (this.doencas == null) {
+		//	this.doencas = new ArrayList<>();
+		//}
+		
+		this.doencas.add(doenca);
+	}
+	
+}
--- lista04/src/lista04/Principal.java
+++ lista04/src/lista04/Principal.java
@@ -16,19 +16,91 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String[] opcoes = {"Livro",
+				           "Pontos",
+				           "Compromisso"};
+		boolean continuar = true;
+		do {
+			int opcao = Console.mostrarMenu(opcoes, "Lista 04", null);
+			switch (opcao) {
+				case 1:
+					
+					break;
+					
+				case 2:
+					executarCenario2();
+					break;
+					
+				case 3:
+					executarCenario3();
+					break;
+					
+				case -1:
+					continuar = false;
+					break;
+			}
+		} while (continuar);		
+	}
+	
+	/**
+	 * Executa o Cenário 3.
+	 */
+	private static void executarCenario3() {
 		
-		Livro livro01 = new Livro();
-		livro01.setCodigo("1598FHK");
-		livro01.setTitulo("Core Java 2");
+		Paciente p1 = new Paciente("00021", "Fulano da Silva");
+		Paciente p2 = new Paciente("000171", "Ciclano dos Santos");
+		p2.addHistorico("Dor de cabeça");
 		
-		ArrayList<String> autoresLivro01 = new ArrayList<>();
-		autoresLivro01.add("Cay S. Horstmann");
-		autoresLivro01.add("Gary Cornell");
+	}
+
+	/**
+	 * Executa o Cenário 2.
+	 */
+	private static void executarCenario2() {
 		
-		livro01.setAutores(autoresLivro01);
+		//Ponto na origem, coordenadas (0, 0)
+		Ponto ponto1 = new Ponto();
+		Ponto ponto2 = new Ponto(2d, 5d);
 		
-		livro01.setIsbn("0130819336");
-		livro01.setAno(1999);
+		double dp1p2 = ponto1.distancia(ponto2);
+		System.out.println("dp1p2: " + dp1p2);
+		double dp272 = ponto2.distancia(7d, 2d);
+		System.out.println("dp272: " + dp272);
+		ponto1.setX(10d);
+		ponto1.setY(3d);
+		dp1p2 = ponto1.distancia(ponto2);
+		System.out.println("dp1p2: " + dp1p2);
+		
+	}
+
+	/**
+	 * Cria um Livro pelo teclado.
+	 * 
+	 * @return
+	 */
+	public static Livro criarLivro() {
+		
+		Livro livro = new Livro();
+		
+		String codigo = Console.recuperaTexto("Informe o código:");
+		livro.setCodigo(codigo);
+		String titulo = Console.recuperaTexto("Informe o título");
+		livro.setTitulo(titulo);
+		
+		ArrayList<String> autores = new ArrayList<>();
+		Integer quantidade = Console.recuperaInteiro("Quantos autores?");
+		for (int i = 0; i < quantidade; i++) {
+			String autor = Console.recuperaTexto("Informe o autor " + (i + 1) + ":");
+			autores.add(autor);
+		}
+		livro.setAutores(autores);
+		
+		String isbn = Console.recuperaTexto("Informe o ISBN:");
+		livro.setIsbn(isbn);
+		Integer ano = Console.recuperaInteiro("Informe o ano:");
+		livro.setAno(ano);
+		
+		return livro;
 		
 	}
 
